3487c23842091e0fd40e9574eefee92fafb894cd
Diego Lafuente <tufosa@gmail.com>
2013-05-07 17:17:58 +0200

    corrected some paths

--- qunit/beta-tests.js
+++ qunit/beta-tests.js
@@ -80,6 +80,7 @@ test('tui object', function() {
 	ok(tui, 'tui object exists');
 });
 test('tuiajax object', function() {
+	log("Logging some crap: " + tui);
 	ok(tui, 'tui.ajax object exists');
 });
 
