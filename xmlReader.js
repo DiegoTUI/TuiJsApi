cb76637a8ff40032a0b086163f6ab517b3881413
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-05-10 16:21:19 +0200

    skeleton of xml

---
+++ xmlReader.js
@@ -0,0 +1,53 @@
+'use strict';
+/*
+ * TuiInnovation JS API.
+ * Parametrized string: a string with $keys$ to be replaced by actual params
+ *
+ * Copyright (C) 2013 TuiInnovation.
+ */
+
+if (typeof tui === 'undefined') {
+	console.log("parametrizedString.js - requiring tui");
+	var tui = require ('/js/tui.js');
+}
+
+/**
+ * The parametrized string.
+ * baseString: the string with the $keys$ in it
+ * parameters: a dictionary with keys and values to be replaced in the baseString
+ */
+tui.parametrizedString = function(baseString, parameters)
+{
+	// self-reference
+	var self = this;
+
+	/**
+	 * Returns baseString with all the parameters replaced
+	 */
+	self.replaceAll = function() {
+		var replaced = baseString;
+
+		if (baseString && parameters) {
+			for (var key in parameters) {
+				replaced = replaced.replace("$"+key+"$",parameters[key]);
+			}
+		}
+
+		return replaced;
+	}
+
+	/**
+	 * Returns an array of strings with the $keys$ that are in baseString, but not in parameters
+	 */
+	 self.getLooseKeys = function() {
+	 	var replaced = self.replaceAll();
+	 	if (typeof replaced != 'undefined')
+	 		return replaced.match(/\$[^\$]+\$/);
+	 	return replaced;
+	 }
+
+	return self;
+}
+
+module.exports = tui.parametrizedString;
+
