d991009c81b9eee858c14a7587b16dc24ba77f48
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-05-13 17:55:08 +0200

    xml reader and tests. Ready to try.

--- qunit/xmlreader-tests.js
+++ qunit/xmlreader-tests.js
@@ -141,7 +141,7 @@ test('a capon', function() {
 	var xmlobject = $(xmlString);
 	xmlobject.find("ServiceTicket").each(function(){
 		tui.debug ("Entered ServiceTicket");
-		$(this).find("TicketInfo ImageList").each(function(){
+		$(this).find("TicketInfo ImageList Image").each(function(){
 			tui.debug("Node name: " + this.nodeName);
 			$(this).find("Type").each(function(){
 				tui.debug("Inner Node name: " + this.nodeName);
