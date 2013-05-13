2ee2c37c2f5f6b3b7d01661edc3a922708c3354e
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-05-13 14:07:53 +0200

    xml reader and tests. Ready to try.

--- qunit/xmlreader-tests.js
+++ qunit/xmlreader-tests.js
@@ -13,77 +13,77 @@ var xmlString = '<TicketAvailRS xsi:schemaLocation="http://www.hotelbeds.com/sch
 		<ServerName>FORM</ServerName> \
 		<ServerId>FO</ServerId> \
 		<SchemaRelease>2005/06</SchemaRelease>  \
-		<HydraCoreRelease>2.0.201304221213</HydraCoreRelease>
-		<HydraEnumerationsRelease>1.0.201304221213</HydraEnumerationsRelease>
-		<MerlinRelease>N/A</MerlinRelease>
-	</AuditData>
-	<PaginationData currentPage="1" totalPages="2"/>
-	<ServiceTicket xsi:type="ServiceTicket" availToken="9ey6mENxtyujqkVKnqvpMA==">
-		<DateFrom date="DateFrom1"/>
-		<DateTo date="DateTo1"/>
-		<Currency code="EUR1">Euro1</Currency>
-		<TicketInfo xsi:type="ProductTicket">
-			<Code>000200515</Code>
-			<Name>Ticket1</Name>
-			<DescriptionList>
-				<Description type="generalDescription" languageCode="ENG">Description 1</Description>
-				<Description type="generalDescription" languageCode="SPA">Descripcion 2</Description>
-			</DescriptionList>
-			<ImageList>
-				<Image>
-					<Type>L</Type>
-					<Order>0</Order>
-					<VisualizationOrder>0</VisualizationOrder>
-					<Url>Image1</Url>
-				</Image>
-				<Image>
-					<Type>S</Type>
-					<Order>0</Order>
-					<VisualizationOrder>0</VisualizationOrder>
-					<Url>Image2</Url>
-				</Image>
-				<Image>
-					<Type>S</Type>
-					<Order>0</Order>
-					<VisualizationOrder>0</VisualizationOrder>
-					<Url>Image3</Url>
-				</Image>
-			</ImageList>
-		</TicketInfo>
-	</ServiceTicket>
-	<ServiceTicket xsi:type="ServiceTicket" availToken="9ey6mENxtyujqkVKnqvpMA==">
-		<DateFrom date="DateFrom2"/>
-		<DateTo date="DateTo2"/>
-		<Currency code="EUR2">Euro2</Currency>
-		<TicketInfo xsi:type="ProductTicket">
-			<Code>000200515</Code>
-			<Name>Ticket2</Name>
-			<DescriptionList>
-				<Description type="generalDescription" languageCode="ENG">Description 1</Description>
-				<Description type="generalDescription" languageCode="SPA">Descripcion 2</Description>
-			</DescriptionList>
-			<ImageList>
-				<Image>
-					<Type>L</Type>
-					<Order>0</Order>
-					<VisualizationOrder>0</VisualizationOrder>
-					<Url>Image1</Url>
-				</Image>
-				<Image>
-					<Type>S</Type>
-					<Order>0</Order>
-					<VisualizationOrder>0</VisualizationOrder>
-					<Url>Image2</Url>
-				</Image>
-				<Image>
-					<Type>S</Type>
-					<Order>0</Order>
-					<VisualizationOrder>0</VisualizationOrder>
-					<Url>Image3</Url>
-				</Image>
-			</ImageList>
-		</TicketInfo>
-	</ServiceTicket>
+		<HydraCoreRelease>2.0.201304221213</HydraCoreRelease> \
+		<HydraEnumerationsRelease>1.0.201304221213</HydraEnumerationsRelease> \
+		<MerlinRelease>N/A</MerlinRelease> \
+	</AuditData> \
+	<PaginationData currentPage="1" totalPages="2"/> \
+	<ServiceTicket xsi:type="ServiceTicket" availToken="9ey6mENxtyujqkVKnqvpMA=="> \
+		<DateFrom date="DateFrom1"/> \
+		<DateTo date="DateTo1"/> \
+		<Currency code="EUR1">Euro1</Currency> \
+		<TicketInfo xsi:type="ProductTicket"> \
+			<Code>000200515</Code> \
+			<Name>Ticket1</Name> \
+			<DescriptionList> \
+				<Description type="generalDescription" languageCode="ENG">Description 1</Description> \
+				<Description type="generalDescription" languageCode="SPA">Descripcion 2</Description> \
+			</DescriptionList> \
+			<ImageList> \
+				<Image> \
+					<Type>L</Type> \
+					<Order>0</Order> \
+					<VisualizationOrder>0</VisualizationOrder> \
+					<Url>Image1</Url> \
+				</Image> \
+				<Image> \
+					<Type>S</Type> \
+					<Order>0</Order> \
+					<VisualizationOrder>0</VisualizationOrder> \
+					<Url>Image2</Url> \
+				</Image> \
+				<Image> \
+					<Type>S</Type> \
+					<Order>0</Order> \
+					<VisualizationOrder>0</VisualizationOrder> \
+					<Url>Image3</Url> \
+				</Image> \
+			</ImageList> \
+		</TicketInfo> \
+	</ServiceTicket> \
+	<ServiceTicket xsi:type="ServiceTicket" availToken="9ey6mENxtyujqkVKnqvpMA=="> \
+		<DateFrom date="DateFrom2"/> \
+		<DateTo date="DateTo2"/> \
+		<Currency code="EUR2">Euro2</Currency> \
+		<TicketInfo xsi:type="ProductTicket"> \
+			<Code>000200515</Code> \
+			<Name>Ticket2</Name> \
+			<DescriptionList> \
+				<Description type="generalDescription" languageCode="ENG">Description 1</Description> \
+				<Description type="generalDescription" languageCode="SPA">Descripcion 2</Description> \
+			</DescriptionList> \
+			<ImageList> \
+				<Image> \
+					<Type>L</Type> \
+					<Order>0</Order> \
+					<VisualizationOrder>0</VisualizationOrder> \
+					<Url>Image1</Url> \
+				</Image> \
+				<Image> \
+					<Type>S</Type> \
+					<Order>0</Order> \
+					<VisualizationOrder>0</VisualizationOrder> \
+					<Url>Image2</Url> \
+				</Image> \
+				<Image> \
+					<Type>S</Type> \
+					<Order>0</Order> \
+					<VisualizationOrder>0</VisualizationOrder> \
+					<Url>Image3</Url> \
+				</Image> \
+			</ImageList> \
+		</TicketInfo> \
+	</ServiceTicket> \
 </TicketAvailRS>';
 
 var descriptionMap = [
