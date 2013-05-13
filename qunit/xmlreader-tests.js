0db27441873a7f0bf158216a73dd838635d4f07d
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-05-13 13:54:18 +0200

    xml reader and tests. Ready to try.

---
+++ qunit/xmlreader-tests.js
@@ -0,0 +1,128 @@
+/**
+ * TuiInnovation.
+ * Xml Reader unit tests.
+ *
+ * Copyright (C) 2013 TuiInnovation.
+ */
+
+var xmlString = 
+'<TicketAvailRS xsi:schemaLocation="http://www.hotelbeds.com/schemas/2005/06/messages TicketAvailRS.xsd" totalItems="27" echoToken="DummyEchoToken">
+	<AuditData>
+		<ProcessTime>647</ProcessTime>
+		<Timestamp>2013-05-13 10:49:38.031</Timestamp>
+		<RequestHost>10.162.29.83</RequestHost>
+		<ServerName>FORM</ServerName>
+		<ServerId>FO</ServerId>
+		<SchemaRelease>2005/06</SchemaRelease>
+		<HydraCoreRelease>2.0.201304221213</HydraCoreRelease>
+		<HydraEnumerationsRelease>1.0.201304221213</HydraEnumerationsRelease>
+		<MerlinRelease>N/A</MerlinRelease>
+	</AuditData>
+	<PaginationData currentPage="1" totalPages="2"/>
+	<ServiceTicket xsi:type="ServiceTicket" availToken="9ey6mENxtyujqkVKnqvpMA==">
+		<DateFrom date="DateFrom1"/>
+		<DateTo date="DateTo1"/>
+		<Currency code="EUR1">Euro1</Currency>
+		<TicketInfo xsi:type="ProductTicket">
+			<Code>000200515</Code>
+			<Name>Ticket1</Name>
+			<DescriptionList>
+				<Description type="generalDescription" languageCode="ENG">Description 1</Description>
+				<Description type="generalDescription" languageCode="SPA">Descripcion 2</Description>
+			</DescriptionList>
+			<ImageList>
+				<Image>
+					<Type>L</Type>
+					<Order>0</Order>
+					<VisualizationOrder>0</VisualizationOrder>
+					<Url>Image1</Url>
+				</Image>
+				<Image>
+					<Type>S</Type>
+					<Order>0</Order>
+					<VisualizationOrder>0</VisualizationOrder>
+					<Url>Image2</Url>
+				</Image>
+				<Image>
+					<Type>S</Type>
+					<Order>0</Order>
+					<VisualizationOrder>0</VisualizationOrder>
+					<Url>Image3</Url>
+				</Image>
+			</ImageList>
+		</TicketInfo>
+	</ServiceTicket>
+	<ServiceTicket xsi:type="ServiceTicket" availToken="9ey6mENxtyujqkVKnqvpMA==">
+		<DateFrom date="DateFrom2"/>
+		<DateTo date="DateTo2"/>
+		<Currency code="EUR2">Euro2</Currency>
+		<TicketInfo xsi:type="ProductTicket">
+			<Code>000200515</Code>
+			<Name>Ticket2</Name>
+			<DescriptionList>
+				<Description type="generalDescription" languageCode="ENG">Description 1</Description>
+				<Description type="generalDescription" languageCode="SPA">Descripcion 2</Description>
+			</DescriptionList>
+			<ImageList>
+				<Image>
+					<Type>L</Type>
+					<Order>0</Order>
+					<VisualizationOrder>0</VisualizationOrder>
+					<Url>Image1</Url>
+				</Image>
+				<Image>
+					<Type>S</Type>
+					<Order>0</Order>
+					<VisualizationOrder>0</VisualizationOrder>
+					<Url>Image2</Url>
+				</Image>
+				<Image>
+					<Type>S</Type>
+					<Order>0</Order>
+					<VisualizationOrder>0</VisualizationOrder>
+					<Url>Image3</Url>
+				</Image>
+			</ImageList>
+		</TicketInfo>
+	</ServiceTicket>
+</TicketAvailRS>';
+
+var descriptionMap = [
+'DateFrom',
+'DateTo',
+'Currency',
+{'CurrencyCode': 'Currency.@code'},
+{'Name': 'TicketInfo.Name'},
+{'TicketInfo.ImageList': [{'Type': 'Image.Type',
+							'Url': 'Image.Url'}]},
+{'TicketInfo.DescriptionList':[{'Type': 'Description.@type',
+					 			'Description': 'Description'}]}];
+
+/**
+ * Run tests. Parse the above xml with the above descriptionMap and see what we got
+ */
+QUnit.module('xmlreader');
+test('parsing test xml', function() {
+	var xmlReader = new tui.xmlreader (xmlString, descriptionMap);
+	var parsedXml = xmlReader.readObjects('ServiceTicket');
+	//Now chek some stuff about the parsed xml
+	ok(parsedXml instanceof Array, 'parsedXml should be an array');
+	ok(parsedXml.length === 2, 'parsedXml should have 2 elements');
+	ok(parsedXml[0].dateFrom === 'DateFrom1', 'dateFrom incorrect');
+	ok(parsedXml[0].dateTo === 'DateTo1', 'dateTo incorrect');
+	ok(parsedXml[1].dateFrom === 'DateFrom2', 'dateFrom incorrect');
+	ok(parsedXml[1].dateTo === 'DateTo2', 'dateTo incorrect');
+	ok(parsedXml[0].Currency === 'Euro1', 'Currency incorrect');
+	ok(parsedXml[0].CurrencyCode === 'EUR1', 'CurrencyCode incorrect');
+	ok(parsedXml[1].Currency === 'Euro2', 'Currency incorrect');
+	ok(parsedXml[1].CurrencyCode === 'EUR2', 'CurrencyCode incorrect');
+	ok(parsedXml[0].Name === 'Ticket1', 'Name incorrect');
+	ok(parsedXml[1].Name === 'Ticket2', 'Name incorrect');
+	for (var i=0; i<parsedXml.length; i++) {
+		var ImageList = parsedXml[i]['TicketInfo.ImageList'];
+		var DescriptionList = parsedXml[i]['TicketInfo.DescriptionList'];
+		ok(imageList.length === 3, 'Only 3 images in the list');
+		ok(DescriptionList.length === 2, 'Only 2 descriptions in the list');
+	}
+	
+});
--- xmlReader.js
+++ xmlReader.js
@@ -1,53 +1,122 @@
 'use strict';
 /*
  * TuiInnovation JS API.
- * Parametrized string: a string with $keys$ to be replaced by actual params
+ * XML Reader: receives an xml string and a description map and returns an array of objects
  *
  * Copyright (C) 2013 TuiInnovation.
  */
 
 if (typeof tui === 'undefined') {
-	console.log("parametrizedString.js - requiring tui");
+	console.log("xmlReader.js - requiring tui");
 	var tui = require ('/js/tui.js');
 }
 
 /**
- * The parametrized string.
- * baseString: the string with the $keys$ in it
- * parameters: a dictionary with keys and values to be replaced in the baseString
+ * The XML reader.
+ * xmlString: the xml in string format
+ * descriptionMap: an array representing the values to be extracted from the xml.
+ * see xmlreader-tests to fully understand this class
  */
-tui.parametrizedString = function(baseString, parameters)
+tui.xmlReader = function(xmlString, descriptionMap)
 {
 	// self-reference
 	var self = this;
 
 	/**
-	 * Returns baseString with all the parameters replaced
+	 * Reads the objects from the xmlString using the descriptionMap
+	 * Returns an array of JS objects
+	 * tag: the tag representing the objects in the xml to be read
 	 */
-	self.replaceAll = function() {
-		var replaced = baseString;
-
-		if (baseString && parameters) {
-			for (var key in parameters) {
-				replaced = replaced.replace("$"+key+"$",parameters[key]);
-			}
-		}
-
-		return replaced;
+	self.readObjects = function(tag) {
+		//initialize result
+		var result =[];
+		//wrap the string in a jquery object
+		var xmlObject = $(xmlString);
+		//parse it
+		xmlObject.find(tag).each(function() {
+			result.push(processElement($(this)));
+		});
+		
+		return result;
 	}
 
 	/**
-	 * Returns an array of strings with the $keys$ that are in baseString, but not in parameters
+	 * Process an element of the xml according to the description Map and returns an object
+	 * element: a jquery object containing the element to be processed
 	 */
-	 self.getLooseKeys = function() {
-	 	var replaced = self.replaceAll();
-	 	if (typeof replaced != 'undefined')
-	 		return replaced.match(/\$[^\$]+\$/);
-	 	return replaced;
-	 }
+	function processElement(element) {
+		//initialize result
+		var result = {};
+		//iterate descriptionMap
+		for (var i=0; i<descriptionMap.length; i++) {
+			var item = descriptionMap[i];
+			if (typeof item === 'string') {	//It's a string
+				result[item] = element.find(item).text();
+			} 
+			else if (typeof item === 'object') {	//It's a dictionary
+				//get the first (and only) key of the dictionary
+				for (var key in item) {
+					var value = item[key];
+					if (value instanceof Array) {	//It's a list
+						//initialize list
+						result[key] = [];
+						//The array should contain only one element and it should be a dictionary
+						if (value.length != 1) tui.error ("Malformed descriptionMap. More than 1 element in inner array: " + value);
+						var innerObject = value[0];
+						//get in the list
+						valueInXml(element,key).each(function(){
+							var elementInList = {};
+							for (var innerKey in innerObject) {
+								elementInList[innerkey] = valueInXml($(this), innerObject[innerKey]);
+							}
+							result[key].push(elementInList);
+						});
+					}
+					else if (typeof value === 'string') {	//It's a deep value
+						result[key] = valueInXml(element, value);
+					}
+					break;	//we only consider the first key
+				}
+			} 
+		}
+		return result;
+	}
+
+	/**
+	 * Explores an xml jQuery object and returns the value in path
+	 * xmlObject: a jquery object containing the xml to look in
+	 * path: a string like "Description.@languageCode" containing the path to look in. "@" is for attributes
+	 */
+	function valueInXml (xmlObject, path) {
+		var elementsInPath = path.split('.');
+		for (var i=0; i<elementsInPath.length; i++) {
+			var element = elementsInPath[i];
+			//Is it an attribute?
+			if (element.startsWith('@')) {
+				return xmlObject.attr(element.substringFrom('@'));
+			}
+			//It is not an attribute. Go deeper.
+			xmlObject = xmlObject.find(element);
+		}
+		return xmlObject.text();
+	}
+
+	/**
+	 * Explores an xml jQuery object and returns the inner object in path
+	 * xmlObject: a jquery object containing the xml to look in
+	 * path: a string like "Description.Code" containing the path to look in.
+	 */
+	function objectInXml (xmlObject, path) {
+		var elementsInPath = path.split('.');
+		for (var i=0; i<elementsInPath.length; i++) {
+			var element = elementsInPath[i];
+			xmlObject = xmlObject.find(element);
+		}
+		return xmlObject;
+	}
 
 	return self;
 }
 
-module.exports = tui.parametrizedString;
+module.exports = tui.xmlReader;
 
