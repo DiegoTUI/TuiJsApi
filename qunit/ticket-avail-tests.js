60b223a429eed36ced8438c2a5331d77381de52e
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-05-21 19:13:50 +0200

    TicketAvail and tests

--- qunit/ticket-avail-tests.js
+++ qunit/ticket-avail-tests.js
@@ -15,13 +15,16 @@ function ok_ticket_avail_request(data)
 
 /* run tests */
 QUnit.module('ticket-avail');
+tui.debug("Passed QUnit module");
 asyncTest('load destinations', function() {
 	var parameters = {
 		Language: "ENG",
 		Credentials_User: "ISLAS",
 		ServiceOccupancy_AdultCount: "1"
 	};
+	tui.debug("Passed var parameters");
 	var ticketAvailRQ = new tui.ticketAvailRequest(parameters);
+	tui.debug("Passed var ticketAvailRQ");
 	ticketAvailRQ.sendRequest(ok_ticket_avail_request, nok);
 });
 
