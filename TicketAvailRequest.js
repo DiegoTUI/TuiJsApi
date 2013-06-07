6645f109674acac5ed4208f755b571a400fa4bd0
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-07 13:36:13 +0200

    refining ticketAvail tests

--- TicketAvailRequest.js
+++ TicketAvailRequest.js
@@ -49,8 +49,8 @@ tuins.TicketAvailRequest = function(queryParameters, descriptionMap, tag)
 	function initParams() {
 		//tui.debug("atlasDefaults for ticketAvailRequest: " + JSON.stringify(tui.atlasDefaults.ticketAvailRequest)); 
 		for (var key in tui.atlasDefaults.ticketAvailRequest) {
-			if (!(key in parameters)){
-				parameters[key] = typeof tui.atlasDefaults.ticketAvailRequest[key] === "function" ?
+			if (!(key in queryParameters)){
+				queryParameters[key] = typeof tui.atlasDefaults.ticketAvailRequest[key] === "function" ?
 											tui.atlasDefaults.ticketAvailRequest[key]() : tui.atlasDefaults.ticketAvailRequest[key];
 			}
 		}
